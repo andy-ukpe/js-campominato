7040c5a2d2c84f6d24904831c6898ec55de652c1
andy <andy.ukpe@outlook.com>
2020-06-04 11:48:31 +0200

    esercizio terminato con correzioni

--- script.js
+++ script.js
@@ -9,51 +9,83 @@
 // cioè il numero di volte che l’utente ha inserito un numero consentito.
 
 // VARIABILI FUNZIONI
-var sediciNumeriRandomPc = generatoreAutomatico(1, 100);
+var rangeMaxNumeri = 100;
+var numeroDiBombe = 16;
+var arrayBombe = generatoreAutomatico(rangeMaxNumeri, numeroDiBombe);
+
 var sediciNumeriRandomUtente = inseritoreNumeriUtente(1, 100);
 
 // array con numeri random pc
-console.log(sediciNumeriRandomPc);
+console.log(arrayBombe);
 // array con numeri utente
 inseritoreNumeriUtente();
 console.log(sediciNumeriRandomUtente);
-// messaggio
-inseritoreNumeriUtente();
-  if (messaggio == true){
-  console.log('Hai perso');
-} else {
-  console.log('Hai vinto');
-}
 
 
 
 
 // CHIEDERE ALL'UTENTE DI INSERIRE UN NUMERO TRA 1 E 100 UNO ALLA VOLTA
 
-function inseritoreNumeriUtente(min, max) {
-  var numeriUtente = [];
-  var messaggio = false;
-  var i = 1;
-  while ( i <= 16) {
-    var numeroUtente = prompt('inserire un numero tra 1 e 100');
-    i++
+var numeroImmissioniUtente = 0;
+var bombaBeccata = false;
+
+var numeroMassimoTentativi = rangeMaxNumeri - numeroDiBombe;
+while(bombaBeccata === false && (numeroImmissioniUtente < numeroMassimoTentativi)){
+  var numeroUtente = prompt('dimmi un numero da uno a' + rangeMaxNumeri);
+
+  // se l'utente becca la mina,blocca il ciclo
+  if (verificaSeElementoInArray(numeroUtente, arrayBombe) === true) {
+    bombaBeccata = true;
+    numeroImmissioniUtente++;
   }
-  while (sediciNumeriRandomPc.includes(numeroUtente) && numeriUtente=<16 ){
-    numeriUtente.push(numeroUtente);
-    }
-  if 
-  return massaggio;
+
+}
+if (bombaBeccata === false) {
+  console.log('hai vinto, il tuo punteggio è' + numeroImmissioniUtente)
+} else{
+  var punteggio = numeroImmissioniUtente - 1;
+  console.log('boom, hai perso il tuo punteggio è ' + punteggio)
+}
+// function inseritoreNumeriUtente(min, max) {
+//   var numeriUtente = [];
+//   var messaggio = false;
+//   var i = 1;
+//   while ( i <= 16) {
+//     var numeroUtente = prompt('inserire un numero tra 1 e 100');
+//     i++
+//   }
+//   while (sediciNumeriRandomPc.includes(numeroUtente) && numeriUtente=<16 ){
+//     numeriUtente.push(numeroUtente);
+//     }
+//   if
+//   return massaggio;
 }
 
 
 // CREAZIONE DEL GENERATORE AUTOMATICO TRA 1 E 100.
 
-function generatoreAutomatico(min, max) {
+function generatoreAutomatico(rangeMaxNumeri, numeroDiBombe) {
   var numeriRandom =[];
 
-  for (var i = 1; i <= 16; i++) {
-    var numeroRandom = Math.floor(Math.random() * (max - min) ) + min;
-    numeriRandom.push(numeroRandom);
+  while (numeriRandom.lenght < numeroDiBombe) {
+    var numeroRandom = Math.floor(Math.random() * (rangeMaxNumeri - 1) ) + 1;
+    if (verificaSeElementoInArray(numeroRandom, numeriRandom) === false;){
+        numeriRandom.push(numeroRandom);
+    }
+
   }
   return numeriRandom;
+}
+// verifica se elemento è presente in un array
+function verificaSeElementoInArray(elemento, listaArray){
+  var inArray = false;
+
+  // scorrere l'array in cerca dell'verificaSePresenteElementoArray
+  for(var i = 0; 1 < listaArray.lenght; i++){
+    if(elemento === listaArray[i]){
+      isArray=true;
+    }
+  }
+
+  return inArray;
 }
