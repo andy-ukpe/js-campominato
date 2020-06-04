d5e3eec6a0e01d277b0b85efec4c58e2c09dc4aa
andy <andy.ukpe@outlook.com>
2020-06-04 16:47:43 +0200

    tentativo di creazione controllo doppioni per array bombe.

--- js/script.js
+++ js/script.js
@@ -8,17 +8,36 @@
 // Al termine della partita il software deve comunicare il punteggio,
 // cioè il numero di volte che l’utente ha inserito un numero consentito.
 
-// Il computer deve generare 16 numeri casuali tra 1 e 100.
-// -->funzione:genera array di 16 numeri casuali tra 1 e 100;
-// -->elementi:mi serve una funzione e un  ciclo for per generare i vari elementi della stringa;
-//  -->ritorna: i numeri arrai completo dove invoca la funzione;
+
 
 // variabili generali
 var rangeMaxNumeri = 100;
 var numeroDiBombe = 16;
 
-var bombe = creazioneArrayBombe(rangeMaxNumeri, numeroDiBombe);
+var listaArrayBombe = creazioneArrayBombe(rangeMaxNumeri, numeroDiBombe);
+
+var bombe = verificaSeElementoInArray(listaArrayBombe[i], listaArrayBombe)
 console.log(bombe);
+
+// In seguito deve chiedere all’utente di inserire un numero alla volta, sempre compreso tra 1 e 100.
+// -->funzione: chiedere all'utente di inserire un numero per volta tra 1 e 100  ;
+// -->elementi:mi serve un ciclo while per la domanda ;
+//  -->ritorna: i numeri arrai completo dove invoca la funzione;
+
+
+
+
+
+
+
+
+
+
+
+// Il computer deve generare 16 numeri casuali tra 1 e 100.
+// -->funzione:genera array di 16 numeri casuali tra 1 e 100;
+// -->elementi:mi serve una funzione e un  ciclo for per generare i vari elementi della stringa;
+//  -->ritorna: i numeri arrai completo dove invoca la funzione;
 
 function creazioneArrayBombe(rangeMaxNumeri, numeroDiBombe){
 
@@ -32,4 +51,22 @@ function creazioneArrayBombe(rangeMaxNumeri, numeroDiBombe){
   }
 
   return arrayBombe;
+}
+
+// verifica che i numeri inseriti non siano doppioni
+// funzione: verificare che all'interno dell'arraybombe non ci siano doppioni;
+// elementi:mi serve un ciclo in cui verificare che i numeri generati dalla fnzione bombe non siano doppi;
+// ritorna: lista array con elementi diversi l'uno dall'altro;
+
+function verificaSeElementoInArray(elementoDaVerificare, listaArray){
+  var inArray = false;
+
+  // scorrere l'array in cerca dell'verificaSePresenteElementoArray
+  for (var i = 0; i < listaArray.length; i++) {
+    if( elemento === listaArray[i] ){
+      inArray = true;
+    }
+
+  }
+    return inArray;
 }
