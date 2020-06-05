b67001b247a6bef63c18a1b44f91b5b59ed7922e
andy <andy.ukpe@outlook.com>
2020-06-05 02:50:44 +0200

    esercizio completato e validato

--- js/script.js
+++ js/script.js
@@ -8,30 +8,83 @@
 // Al termine della partita il software deve comunicare il punteggio,
 // cioè il numero di volte che l’utente ha inserito un numero consentito.
 
-
+// richiedere livello di difficolta
+var difficolta = parseInt(prompt('inserisci un numero da 0 a 2 per scegliere la difficoltà'));
 
 // variabili generali
-var rangeMaxNumeri = 100;
+var rangeMaxNumeri = calcolareLivelloGioco(difficolta);
 var numeroDiBombe = 16;
+var tentativi = rangeMaxNumeri - numeroDiBombe;
+var arrayNumeriUtente = [];
+
+
 
 var listaArrayBombe = creazioneArrayBombe(rangeMaxNumeri, numeroDiBombe);
-
-var bombe = verificaSeElementoInArray(listaArrayBombe[i], listaArrayBombe)
-console.log(bombe);
+console.log(listaArrayBombe);
 
 // In seguito deve chiedere all’utente di inserire un numero alla volta, sempre compreso tra 1 e 100.
-// -->funzione: chiedere all'utente di inserire un numero per volta tra 1 e 100  ;
-// -->elementi:mi serve un ciclo while per la domanda ;
-//  -->ritorna: i numeri arrai completo dove invoca la funzione;
+// -->funzione: chiedere all'utente di inserire un numero per volta tra 1 e 100, con un massimo di tentativi(100-16)  ;
+// -->elementi:mi serve un ciclo while per la domanda, un array per collezionare gli input;
+//  -->ritorna: l'array con il numeri inseri dall'utente e il risultato della partita;
+var bombaBeccata = false;
+var inputUtente = 0;
 
+while (bombaBeccata === false && inputUtente < tentativi) {
+  // richiesta inserimento
+  var numeroUtente = parseInt(prompt(' Inserisci un numero tra 1 e ' + rangeMaxNumeri ));
+  // verifica inserimento
+  while((numeroUtente < 1) || (numeroUtente > rangeMaxNumeri) || isNaN(numeroUtente)){
+    var messaggio;
+    numeroUtente = parseInt(prompt(' Errore! Inserisci un numero tra 1 e '+ rangeMaxNumeri ));
 
+  }
+  while (verificaSeElementoInArray(numeroUtente, arrayNumeriUtente) === true ) {
+    numeroUtente = parseInt(prompt(' Errore! Hai già inserito questo numero: Inserisci un numero tra 1 e '+ rangeMaxNumeri ));
+  }
+  // stringa if per capire se la bomba è stata beccata o meno
+  if (verificaSeElementoInArray(numeroUtente, listaArrayBombe) === true ) {
+    bombaBeccata = true;
+    arrayNumeriUtente.push(numeroUtente);
+    inputUtente++;
+  } else {
+    arrayNumeriUtente.push(numeroUtente);
+    inputUtente++;
+  }
 
+}
+console.log(bombaBeccata);
+console.log(arrayNumeriUtente);
 
+if(bombaBeccata === false){
+  alert('hai vinto, il tuo punteggio è ' + inputUtente +
+  '. I tuoi numeri sono: ' + arrayNumeriUtente +
+  '. Le bombe erano rappresentate dai numeri: ' + listaArrayBombe);
+}else {
+  var punteggio = inputUtente - 1;
+  alert('boom, hai perso il tuo punteggio è ' + punteggio +
+  '. il numero che hai inserito è ' + arrayNumeriUtente +
+  '. Le bombe erano rappresentate dai numeri: ' + listaArrayBombe);
+}
 
+// funzione livello
+function calcolareLivelloGioco(difficolta){
+  var numberRange;
 
-
-
-
+  switch (difficolta) {
+    case 0:
+      numberRange = 100;
+      break;
+      case 1:
+        numberRange = 80;
+        break;
+        case 2:
+          numberRange = 50;
+          break;
+    default:
+      numberRange = 100;
+  }
+  return numberRange;
+}
 
 
 // Il computer deve generare 16 numeri casuali tra 1 e 100.
@@ -46,8 +99,9 @@ function creazioneArrayBombe(rangeMaxNumeri, numeroDiBombe){
   while(arrayBombe.length < numeroDiBombe){
 
     var numeroRandom = Math.floor(Math.random() * (rangeMaxNumeri - 1) ) + 1 ;
-    arrayBombe.push(numeroRandom);
-
+     if (!verificaSeElementoInArray(numeroRandom, arrayBombe)) {
+       arrayBombe.push(numeroRandom);
+     }
   }
 
   return arrayBombe;
@@ -63,7 +117,7 @@ function verificaSeElementoInArray(elementoDaVerificare, listaArray){
 
   // scorrere l'array in cerca dell'verificaSePresenteElementoArray
   for (var i = 0; i < listaArray.length; i++) {
-    if( elemento === listaArray[i] ){
+    if( elementoDaVerificare === listaArray[i] ){
       inArray = true;
     }
 
